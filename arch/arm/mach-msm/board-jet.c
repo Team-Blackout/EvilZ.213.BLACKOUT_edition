c4303d4281821c45e9482c14c2dfe0b5af7200a9
Bryan Smith <zarboz@gmail.com>
2012-09-21 17:22:17 -0600

    adjusting flash limit to 2% battery and adjusting key backlight to 25

--- arch/arm/mach-msm/board-jet.c
+++ arch/arm/mach-msm/board-jet.c
@@ -1602,7 +1602,7 @@ static struct camera_flash_info msm_camera_sensor_s5k3h2yx_flash_info = {
 
 static struct camera_flash_cfg msm_camera_sensor_s5k3h2yx_flash_cfg = {
 	.low_temp_limit		= 5,
-	.low_cap_limit		= 15,
+	.low_cap_limit		= 2,
 	.flash_info             = &msm_camera_sensor_s5k3h2yx_flash_info,
 };
 /* Andrew_Cheng linear led 20111205 ME */
@@ -5569,7 +5569,7 @@ static struct pm8xxx_led_configure pm8921_led_info[] = {
 		.duites_size 	= 8,
 		.duty_time_ms 	= 64,
 		.lut_flag 	= PM_PWM_LUT_RAMP_UP | PM_PWM_LUT_PAUSE_HI_EN,
-		.out_current    = 40,
+		.out_current    = 25,
 		.duties		= {0, 15, 30, 45, 60, 75, 90, 100,
 				100, 90, 75, 60, 45, 30, 15, 0,
 				0, 0, 0, 0, 0, 0, 0, 0,

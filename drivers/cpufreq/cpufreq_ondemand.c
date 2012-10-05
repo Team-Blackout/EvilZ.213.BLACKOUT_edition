4a2cfa18708e03bc3bf982ed0f56a2960b27a58d
Steve Kondik <shade@chemlab.org>
2012-10-05 01:26:36 -0700

    drivers/cpufreq: Add max duration for boosting
    Change-Id: I87b180ecf52bf1dfe8e3bfe3be8731fea888e600

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -41,6 +41,11 @@
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
 #define MIN_FREQUENCY_DOWN_DIFFERENTIAL		(1)
+<<<<<<< HEAD
+=======
+#define DEFAULT_FREQ_BOOST_TIME			(500000)
+#define MAX_FREQ_BOOST_TIME				(5000000)
+>>>>>>> 7b1169b... drivers/cpufreq: Add max duration for boosting
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
 #define DBS_INPUT_EVENT_MIN_FREQ		(dbs_tuners_ins.two_phase_freq)
@@ -312,7 +317,36 @@ static ssize_t show_powersave_bias
 	return snprintf(buf, PAGE_SIZE, "%d\n", dbs_tuners_ins.powersave_bias);
 }
 
+<<<<<<< HEAD
 static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
+=======
+static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
+				const char *buf, size_t count)
+{
+	int ret;
+	unsigned int input;
+
+	ret = sscanf(buf, "%u", &input);
+	if (ret < 0)
+		return ret;
+
+	if (input > 1 && input <= MAX_FREQ_BOOST_TIME)
+		dbs_tuners_ins.freq_boost_time = input;
+	else
+		dbs_tuners_ins.freq_boost_time = DEFAULT_FREQ_BOOST_TIME;
+
+	dbs_tuners_ins.boosted = 1;
+	freq_boosted_time = ktime_to_us(ktime_get());
+
+	if (sampling_rate_boosted) {
+		sampling_rate_boosted = 0;
+		dbs_tuners_ins.sampling_rate = current_sampling_rate;
+	}
+	return count;
+}
+
+static ssize_t store_boostfreq(struct kobject *a, struct attribute *b,
+>>>>>>> 7b1169b... drivers/cpufreq: Add max duration for boosting
 				   const char *buf, size_t count)
 {
 	unsigned int input;

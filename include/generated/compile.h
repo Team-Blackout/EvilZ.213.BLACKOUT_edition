bb78f9aa6f21a5da80a6d9eb29392cada532ce72
Bryan Smith <zarboz@gmail.com>
2012-11-10 09:24:34 -0800

    patched to 3.0.40->3.0.41

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 93 */
+/* This file is auto generated, version 94 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#93 SMP PREEMPT Sun Nov 11 11:24:22 PST 2012"
+#define UTS_VERSION "#94 SMP PREEMPT Sun Nov 11 12:02:09 PST 2012"
 #define LINUX_COMPILE_BY "zarboz"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.7.3 20121001 (prerelease) (Linaro GCC 4.7-2012.10) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.40-rc1-g0162a2a-dirty"
+#define UTS_RELEASE "3.0.41-rc1-g3f608a8-dirty"

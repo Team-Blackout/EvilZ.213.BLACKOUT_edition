3b585b6ba2990a1efcfae3246fb5657455fcc51b
Bryan Smith <zarboz@gmail.com>
2012-11-11 09:39:49 -0800

    udpating version

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 83 */
+/* This file is auto generated, version 85 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#83 SMP PREEMPT Sun Nov 11 08:45:04 PST 2012"
+#define UTS_VERSION "#85 SMP PREEMPT Sun Nov 11 09:30:44 PST 2012"
 #define LINUX_COMPILE_BY "zarboz"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.7.3 20121001 (prerelease) (Linaro GCC 4.7-2012.10) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.29-rc1-gc3b4c74-dirty"
+#define UTS_RELEASE "3.0.31-rc1-g441bd3a-dirty"
